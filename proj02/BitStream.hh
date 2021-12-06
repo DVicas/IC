7a8ad8978f70e16989ba65d3e2affc17c76dcec5
Bernardo Falé <mbfale@ua.pt>
2021-12-06 15:28:54 +0000

    1 & 2 full done

--- proj02/BitStream.hh
+++ proj02/BitStream.hh
@@ -9,38 +9,87 @@ class BitStream{
     public:
     
         BitStream(string inFile = "",string outFile = "");
-        void writeBit(void);
-        void readBit(void);
+        void writeBit(int bit);
+        int readBit(void);
+        void writeBits(vector<int> n_bits);
+        vector<int> readBits(int bits);
         vector<int> getBuffer(void);
         void close(void);
 
     private:
         vector<int> buffer;
-        fstream in;
-        fstream out;
+        fstream in, out;
 };
 
 BitStream::BitStream(string inFile,string outFile){
     //bitstream constructor
     if(not inFile.empty()){
         in.open(inFile);
-    }
-    if(not outFile.empty()){
-        out.open(outFile);
-    }
-}
-void BitStream::writeBit(){
-    //BitStream writeBit method
-}
-void BitStream::readBit(){
-    //BitStream readBit
-    char c;
-    while(in.get(c)){
-        for(int i = 7; i >=0; i--){
-            buffer.push_back(((c >> i) & 1));
+        char c;
+        while(in.get(c)){
+            for(int i = 7; i >=0; i--){
+                buffer.push_back(((c >> i) & 1));
+            }
         }
     }
+    if(not outFile.empty()){
+        out.open(outFile, fstream::out | fstream::app);
+    }
+}
+void BitStream::writeBit(int bit){
+    //BitStream writeBit method
+    if(out.is_open()){
+        out << bit;
+        
+    }else{
+        cout << "No outfile to write to!" << endl;
+    }
+}
+int BitStream::readBit(){
+    //BitStream readBit
+    if(in.is_open()){
+        vector<int> auxiliar;
+        for(int i = 1; i < buffer.size(); i++){
+            auxiliar.push_back(buffer[i]);
+        }
+        int bit = buffer.at(0);
+        buffer = auxiliar;
+        return bit;
+        
+    }else{
+        cout << "No input file to read!" << endl;
+        return 2;
+    }
+}
+void BitStream::writeBits(vector<int> n_bits){
+    //BitStream write n bits
+    if(out.is_open()){
+        for(int x: n_bits){
+            out << x;
+        }
+    }else{
+        cout << "No output file to write into!" << endl;
+    }
+}
+vector<int> BitStream::readBits(int bits){
+    //BitStream read n bits
+    vector<int> nbits;
 
+    if(in.is_open()){
+        vector<int> auxiliar;
+        for(int i = bits; i < buffer.size(); i++){
+            auxiliar.push_back(buffer[i]);
+        }
+        for(int x=0; x < bits; x++){
+            nbits.push_back(buffer[x]);
+        }
+        buffer = auxiliar;
+        return nbits;
+        
+    }else{
+        cout << "No input file to read!" << endl;
+        return nbits;
+    }
 }
 vector<int> BitStream::getBuffer(){
     return buffer;
--- proj02/test_bitstream.cpp
+++ proj02/test_bitstream.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include "BitStream.hh"
 
 using namespace std;
 
 int main(int argc, char* argv[]){
     vector<int> buff;
-    BitStream bstream("teste.txt");
-    bstream.readBit();
-    buff = bstream.getBuffer();
-    for(int x: buff){
-        cout << x;
-    }
+    BitStream bstream("teste.txt","out.txt");
+    cout << bstream.readBit();
+    vector<int> n_bits{1,0,0,1,0};
+    bstream.writeBits(n_bits);
+    //for(int x: bstream.getBuffer()){
+    //    cout << x;
+    //}
     bstream.close();
     return 1;
 }
