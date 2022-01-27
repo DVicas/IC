84690bc316cebf80527adffad1c98dfd4b19ec83
Bernardo Falé <mbfale@ua.pt>
2022-01-27 16:52:51 +0000

    Part A done

--- proj03/Fcm.hh
+++ proj03/Fcm.hh
@@ -5,30 +5,38 @@
 #include <fstream>
 #include <string>
 #include <map>
-#include <algorithm>
+#include <vector>
+#include <cmath>
 
 using namespace std;
-
 
 class Fcm{
     public: 
         
-        Fcm(string file, int k);
+        Fcm(string file, int k, int alpha);
         void openfile();
         void read();
+        double calculate();
         map<string, int> getA();
+        map<string, int> getCtx();
+        map<string, double> getH();
         string lower(string s);
+        void close();
 
     private:
         int k_arg;
+        int smoothing_param;
         string i_file;
         fstream f;
+        map<string, int> contexts;
         map<string, int> alphabet;
+        map<string, double> entropies;
 };
 
-Fcm::Fcm(string file, int k){
+Fcm::Fcm(string file, int k, int alpha){
     k_arg = k;
     i_file = file;
+    smoothing_param = alpha;
 }
 void Fcm::openfile(){
     if(not i_file.empty()){
@@ -41,27 +49,77 @@ void Fcm::read(){
     char c;
     string ctx = "";
     while(f.get(c)){
+        if(c == '\n' or c == '\n') continue;
         ctx += tolower(c);
         string s = lower(string(1, c));
         alphabet[s]++;
- 
-        if(ctx.length() == k_arg){
+        
+        if(ctx.length() == k_arg + 1){
             alphabet[ctx]++;
+            contexts[ctx.substr(0, k_arg)]++;
             ctx = ctx.substr(1);
-        }
-            
+        }     
     }
 }
+
+double Fcm::calculate(){
+    map<string, int>::iterator it;
+    vector<string> aux;
+    map<string, int> all_elems;
+    for(it = alphabet.begin(); it != alphabet.end(); ++it){
+        if(it->first.length() == 1){
+            aux.push_back(it->first);
+        }
+    }
+    double probability = 0.0;
+    double pi = 0.0;
+    int total = 0;
+
+    for(it = contexts.begin(); it != contexts.end(); it++){
+        total += it->second;
+        for(string x : aux){
+            pi = (double) alphabet[it->first + x] / contexts[it->first];
+            if(pi == 0) pi = 0.1; //-TODO : CHANGE THIS 
+            probability += pi * (log(pi)/log(2));
+        }
+        entropies[it->first] = -1 * probability;
+        probability = 0.0;
+    }
+
+    double total_entropy = 0.0;
+    for(it = contexts.begin(); it != contexts.end(); it++){
+        total_entropy += ((double) it->second / total) * entropies[it->first];
+    }
+    return total_entropy;        
+}
+
 map<string, int> Fcm::getA(){
     
     return alphabet;
 }
-
+map<string, int> Fcm::getCtx(){
+    
+    return contexts;
+}
+map<string, double> Fcm::getH(){
+    
+    return entropies;
+}
 string Fcm::lower(string s){
     
-    transform(s.begin(), s.end(), s.begin(),[](char c){ 
-        return tolower(c);});
-    return s;
+    string x;
+
+    for(char elem : s){
+        x += tolower(elem);
+    }
+    return x;
 }
+void Fcm::close(){
+    /**
+    * Useful method to reallocate memory
+    */
 
-
+    if(f.is_open()){
+        f.close();
+    }
+}
--- proj03/fcm.cpp
+++ proj03/fcm.cpp
@@ -7,15 +7,29 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-    Fcm fcm("teste.txt", 3);
+    Fcm fcm("teste.txt", 2, 10);
     fcm.openfile();
     fcm.read();
+    double entropy = fcm.calculate();
+    cout << "Entropy of the text -> " << entropy << endl;
+    fcm.close();
     map<string, int>::iterator it;
-    map<string, int> alph = fcm.getA();
-    for(it = alph.begin(); it != alph.end(); ++it){
-        cout << it->first << '\t' << it->second << endl;
-    }
-
+    map<string, double>::iterator dit;
+    map<string, int> alph = fcm.getCtx();
+    // for(it = alph.begin(); it != alph.end(); ++it){
+    //     if(it->first.length() != 1){
+    //         cout << it->first << '\t' << it->second << endl;
+    //     }   
+    // }
+    // for(auto ctx : fcm.getCtx()){
+    //     //cout << ctx << endl ;
+    // }
     
+    
+    
+    map<string, double> entropies = fcm.getH();
+    for(dit = entropies.begin(); dit != entropies.end(); ++dit){
+        //cout << dit->first << '\t' << dit->second << endl;
+    }   
     return 0;
 }
